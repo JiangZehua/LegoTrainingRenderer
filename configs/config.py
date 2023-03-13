06f08f91940f881909f92f95ac5d1955a1a17c6a
Sam Earle <smearle93@gmail.com>
2023-03-13 01:09:55 -0400

    Some work on SB3 training loop.

--- configs/config.py
+++ configs/config.py
@@ -1,10 +1,13 @@
-
 from dataclasses import dataclass
+from typing import Optional
 
 
 @dataclass
 class Config:
-    map_shape = (5, 5, 5)
-    render = False
-    max_steps = 100
-    brick_size_range = (3, 3, 3)
+    map_shape: tuple = (5, 5, 5)
+    render: bool= False
+    max_steps: int = 100
+    brick_size_range: tuple = (3, 3, 3)
+    save_freq: int = 10
+    log_dir: str = 'logs'
+    exp_dir: Optional[str] = None  # This gets set automatically depending on other parameters
--- train_rl.py
+++ train_rl.py
@@ -1,15 +1,68 @@
-from stable_baselines3 import PPO
+from functools import partial
+import json
+import os
+
+from stable_baselines3 import PPO as PPO_SB3
 
 from configs.config import Config
 from render.render_env import LegoEnv
 
+
+class PPO(PPO_SB3):
+    def __init__(self, cfg: Config, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.cfg = cfg
+        self.n_step = 0
+
+    def save(self, save_dir, *args, **kwargs):
+        path = os.path.join(save_dir, 'model')
+        super().save(path, *args, **kwargs)
+
+        # Convert config to a dict and save.
+        cfg_dict = {k: getattr(cfg, k) for k in dir(cfg) if not k.startswith('_')}
+        with open(os.path.join(save_dir, 'config.json'), 'w') as f:
+            # yaml.dump(cfg)
+            json.dump(cfg_dict, f)
+
+    def load(self, load_dir, *args, **kwargs):
+        cfg_path = os.path.join(load_dir, 'config.json')
+
+        with open(cfg_path, 'rb') as f:
+            cfg_dict = json.load(f)
+
+        cfg = Config(**cfg_dict)
+        model_path = os.path.join(load_dir, 'model')
+
+        # Call this instead of super so that when, inside super's load function, when the model is re-initialized using
+        # loaded data, we also feed 
+        load_cls = partial(PPO_SB3.__init__, cfg=cfg)
+        PPO_SB3.load(path=model_path)
+        # super().load(path=model_path)
+
+
+def init_config(cfg: Config):
+    cfg.exp_dir = os.path.join(cfg.log_dir, 'latest')
+
 def train_callback(*args, **kwargs):
-    breakpoint()
+    model: PPO = args[0]['self']
+    cfg = model.cfg
+    if model.n_step % cfg.save_freq == 0:
+        model.save(save_dir=cfg.exp_dir)
+    model.n_step += 1
 
 def main(cfg: Config):
+    init_config(cfg)
+    if not os.path.isdir(cfg.log_dir):
+        os.makedirs(cfg.log_dir, exist_ok=True)
     env = LegoEnv(cfg=cfg)
-    model = PPO("MlpPolicy", env, verbose=1)
-    model.learn(total_timesteps=50_000)
+    model = PPO(cfg=cfg, policy="MlpPolicy", env=env, verbose=1)
+
+    # Re-load model if it exists.
+    if os.path.exists(cfg.exp_dir):
+        model.load(load_dir = cfg.exp_dir)
+
+    model.learn(total_timesteps=50_000, callback=train_callback)
+
 
 if __name__ == "__main__":
     cfg = Config()
